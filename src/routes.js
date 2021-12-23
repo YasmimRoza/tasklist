9963b2b8713f308f045fba2dea22957b8fc141cc
Yasmim Roza <yasmimroza16@gmail.com>
2021-12-23 03:18:43 -0400

    Feito o controller e route de User, registrado no banco.

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/model/User';
+
+import UserController from './app/controller/UserController';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'Teste2',
-    email: 'teste@teste2.com',
-    password_hash: '1222',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
